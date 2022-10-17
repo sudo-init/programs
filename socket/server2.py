18e467772d8f47cf94692902c389497b0beb76a9
sudo-init <dep.em.k.0905@gmail.com>
2022-10-17 18:49:04 +0900

    modified socket source.

--- socket/server2.py
+++ socket/server2.py
@@ -1,17 +1,41 @@
 import socket
+import logging
 
-IP = ''
-PORT = ''
-SIZE = ''
+IP = '127.0.0.1'
+PORT = 8080
+SIZE = 1024
 ADDR = (IP, PORT)
+
+logging.basicConfig(
+    level=logging.INFO,
+    # format="%(asctime)s [%(levelname)s] %(message)s",
+    format = '[%(asctime)s] %(message)s',
+    # handlers=[
+    #     logging.FileHandler("debug.log"),
+    #     logging.StreamHandler()
+    # ]
+)
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind(ADDR)
     server_socket.listen()
 
-
-    while True:
+    try:
         client_socket, client_addr = server_socket.accept()
-        msg = client_socket.recv(SIZE)
-        
+
+        with client_socket:
+            logging.info(f'Connected by {client_addr}')
+
+            while True:
+                
+                msg = client_socket.recv(SIZE)
+    
+    except Exception as e:
+        logging.exception(e)
+
+    # finally:
+    #     client_socket.close()
+
+    
+
--- socket/test.py
+++ socket/test.py
@@ -2,13 +2,16 @@ import logging
 
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     # format="%(asctime)s [%(levelname)s] %(message)s",
-    format="[%(asctime)s] %(message)s",
+    format = '[%(asctime)s] %(message)s',
     # handlers=[
     #     logging.FileHandler("debug.log"),
     #     logging.StreamHandler()
     # ]
 )
 
-logging.INFO('hello world')
+# logging.INFO('hello world')
+msg = 'hello world'
+print(msg)
+logging.info(msg)
