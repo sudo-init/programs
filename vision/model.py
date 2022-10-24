97f9f41629f3a22051372ab3d78ff93f974d1560
sudo-init <dep.em.k.0905@gmail.com>
2022-10-24 11:25:28 +0900

    vision model commit!

---
+++ vision/model.py
@@ -0,0 +1,18 @@
+
+
+class model:
+    
+    
+    def __init__(self):
+        self.model = self.loadModel()
+    
+    
+    def loadModel(self):
+        
+        pass
+        
+    def detect(self):
+        pass
+    
+
+    
