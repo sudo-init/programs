1c2101fd8389ba49c8879bb2da69dc00962bc513
sudo-init <dep.em.k.0905@gmail.com>
2022-11-07 17:15:18 +0900

    python gui commit!

---
+++ gui/pyqt/pyqt_practice.py
@@ -0,0 +1,34 @@
+
+
+import sys
+
+from PyQt5.QtWidgets import *
+
+
+class Application(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+    
+        self.button_1.clicked.connect(self.button1Function)
+        
+    
+    
+    def button1Function(self):
+        print('Button 1 clicked!')
+    
+    
+    
+if __name__ == '__main__':
+    # app = QApplication(sys.argv)
+    # testApp = Application()
+    # testApp.show()
+    # app.exec_()
+    
+    # app = QApplication([])
+    # label = QLabel('Hello World!')
+    # label.show()
+    # app.exec()
+    
+    app = QApplication(sys.argv)
+    # label
